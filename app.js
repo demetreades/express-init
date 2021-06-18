44cf30ce3af883ef48cfb09cb5a642a80fa03e0a
demetreades <demetreades>
2021-06-18 13:53:07 +0300

    ++fork

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./controllers/index');
+var usersRouter = require('./controllers/users');
 
 var app = express();
+
+// const puplicDirectory = path.join(__dirname, 'public');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
